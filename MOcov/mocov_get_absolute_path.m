b9a96f24f2ae5566a60bc44f5f4ecbd1ac256d78
Joel LeBlanc <jwleblan@gmail.com>
2021-07-14 13:36:49 -0400

    Windows support
    Updated get_absolute_path and tests to support Windows

--- MOcov/mocov_get_absolute_path.m
+++ MOcov/mocov_get_absolute_path.m
@@ -19,8 +19,9 @@ function abs_fn=mocov_get_absolute_path(fn)
     abs_fn=clean_path_string(abs_fn);
 
 function clean_abs_fn=clean_path_string(abs_fn)
-
-    re=regexptranslate('escape',filesep());
+    
+    fs=filesep();
+    re=regexptranslate('escape',fs);
 
     while true
         sp=regexp(abs_fn,re,'split');
@@ -51,15 +52,18 @@ function clean_abs_fn=clean_path_string(abs_fn)
         end
 
         parts=[sp(keep);...
-                    repmat({filesep()},1,sum(keep))];
+                    repmat({fs},1,sum(keep))];
 
-        clean_abs_fn=strrep(cat(2,parts{:}),...
-                            [filesep() filesep()], ...
-                            filesep());
+        clean_abs_fn=strrep(cat(2,parts{:}), [fs fs], fs);
 
         % deal with filesep at the end of the path
-        while numel(clean_abs_fn)>1 && clean_abs_fn(end)==filesep();
-            clean_abs_fn=clean_abs_fn(1:(end-1));
+        while numel(clean_abs_fn)>1 && clean_abs_fn(end)==fs
+            if ispc() && numel(clean_abs_fn)==3 && clean_abs_fn(end-1)==':' 
+                % Do not strip the slash on windows drive letters ( e.g c:\ )
+                break
+            else    
+                clean_abs_fn=clean_abs_fn(1:(end-1));
+            end
         end
 
         if strcmp(clean_abs_fn,abs_fn)
