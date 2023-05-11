59fa877a12e8024769da30168157adb324dfbd17
Remi Gau <remi_gau@hotmail.com>
2023-05-11 07:44:40 -0400

    add regression tests

---
+++ extra_tests/tests/test_is_octave.m
@@ -0,0 +1,11 @@
+function test_suite = test_is_octave %#ok<*STOUT>
+    try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+        test_functions = localfunctions(); %#ok<*NASGU>
+    catch % no problem; early Matlab versions can use initTestSuite fine
+    end
+    initTestSuite;
+end
+
+function test_is_octave_smoke()
+    retval = is_octave;
+end
